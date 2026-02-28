c2870856759ac8dc32e75efcdcc59d2950398188
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2026-02-28 22:33:09 +0200

    added world factory and location class and added all locations

---
+++ Engine/Models/Location.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Location(int xCoordinate, int yCoordinate, string name, string description, string? imageName = null)
+    {
+        public int XCoordinate { get; set; } = xCoordinate;
+        public int YCoordinate { get; set; } = yCoordinate;
+
+        public string Name { get; set; } = name;
+
+        public string Description { get; set; } = description;
+        public string? ImageName { get; set; } = imageName;
+    }
+}
--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -4,17 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Engine.Factories;
 
 namespace Engine.ViewModels
 {
     public class GameSession
     {
+        public World CurrentWorld { get; set; }
         public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
 
         public GameSession()
         {
-            CurrentPlayer = new Player("Yahya", "Fighter");
+            Player CurrentPlayer = new("Yahya", "Fighter");
 
+            WorldFactory factory = new();
+            World CurrentWorld = factory.CreateWorld();
+
+            CurrentLocation = CurrentWorld.LocationAt(0, -1)!;
         }
     }
 }
