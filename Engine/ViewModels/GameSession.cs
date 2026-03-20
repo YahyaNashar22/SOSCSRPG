a68ec91b9a10891b1c6060ab641f22fb84722c5e
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2026-03-20 02:15:24 +0200

    added trader Screen

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -36,6 +36,8 @@ namespace Engine.ViewModels
                 CompleteQuestsAtLocation();
                 GivePlayerQuestsAtLocation();
                 GetMonsterAtLocation();
+
+                CurrentTrader = CurrentLocation.TraderHere;
             }
         }
         public Monster? CurrentMonster
